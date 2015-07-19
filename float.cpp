d21b45b7d2585b23153948e83bef4a68fc21b46d
Yuchen Lin <yuchenlin0212@163.com>
2015-07-19 15:50:50 +0800

    测试了所有的一元函数,并且修改了Float类的精度转换函数

--- float.cpp
+++ float.cpp
@@ -174,12 +174,12 @@ Number* Float::getMin(Number* obj){
 }
 //不知道这两个什么鬼 要先转换成Exact才可以吧
 Number* Float::numerator(){
-    assert(isInteger() and "numerator is only for integer");
-    return truncate();
+//    assert(isInteger() and "numerator is only for integer");
+    return this->toExact()->numerator();
 }
 Number* Float::denominator(){
-    assert(isInteger() and "denominator is only for integer");
-    return new Float(1.0);
+//    assert(isInteger() and "denominator is only for integer");
+    return this->toExact()->denominator();
 }
 
 
